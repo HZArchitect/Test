163a5f8aa089d9380a90a646ad08fab7f9334f9c
lh <657664640@qq.com>
2020-10-17 18:15:06 +0800

    lh:第三次提交:环境监控类初始化

---
+++ hamonitor/src/main/java/sensitive/EnvironmentMonitor.java
@@ -0,0 +1,16 @@
+package sensitive;
+
+import java.util.List;
+
+public class EnvironmentMonitor implements MonitorInterface {
+
+    public List<MonitorStatu> getClusterStatus() {
+
+
+        return null;
+    }
+
+    public void openJob() {
+
+    }
+}
