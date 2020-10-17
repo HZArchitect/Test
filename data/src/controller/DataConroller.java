16185911d09d902da2c4ebc5db79522a25048ddf
lh <657664640@qq.com>
2020-10-17 18:24:14 +0800

    lh:第四次提交:DataController修改

---
+++ data/src/controller/DataConroller.java
@@ -0,0 +1,21 @@
+package controller;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataConroller {
+
+
+    public void addData(){
+        List<String> dataList = Arrays.asList(new String[]{"1a","2b","3c","4d","5e",
+                "6f","7a","8b","daaddd","fasd"});
+
+        System.out.println(dataList);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello cui nao po~~~");
+    }
+
+}
--- data/src/controller/dataConroller.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-
-public class dataConroller {
-
-    public void addData(){
-
-    }
-}
